ce8f44ca6e7409f6f4aa3ef4414c3a5af9b1f782
Eldar199 <elikk_from_ge@mail.ru>
2023-01-17 23:22:39 +0400

    Brought new html and controller implementation

---
+++ src/main/java/com/example/demo/ViewController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String homepage (){
+        return "index";
+    }
+
+
+}
